88e414cbfd8705d4baf9a5cad284193ec19f968c
Oğuzhan Yılmaz <ibrahimoguzhany@gmail.com>
2023-06-18 23:32:00 +0300

    frontend calendar layout improvements

--- frontend/CalendarApp/src/components/Calendar.jsx
+++ frontend/CalendarApp/src/components/Calendar.jsx
@@ -1,150 +1,175 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.css";
-import "../styles.css";
+import "./styles.css";
 import CustomTuiCalendar from "./CustomTuiCalendar";
 import CustomTuiModal from "./CustomTuiModal";
+import { useUser } from "../lib/customHooks";
+import { API_ROUTES } from "../utils/constants";
+import { getTokenFromLocalStorage } from "../lib/common";
 
 const start = new Date();
 const end = new Date(new Date().setMinutes(start.getMinutes() + 60));
 
-const attendees = [
-  {
-    id: "1",
-    name: "Chin"
-  },
-  { id: "2", name: "Khanh" },
-  { id: "3", name: "Linh" },
-  { id: "4", name: "Hai" }
-];
 const schedules = [
   {
     id: "1",
-    title: "Mua nuoc dum",
+    title: "İbrahimle Görüşme",
     calendarId: "1",
     category: "time",
-    attendees: ["Chin"],
     isVisible: true,
     start,
-    end
+    end,
   },
   {
     id: "2",
-    title: "Di lau nha",
+    title: "Oğuzhanla toplantı",
     calendarId: "2",
     category: "time",
-    attendees: ["Khanh"],
     isVisible: true,
     start: new Date(new Date().setHours(start.getHours() + 1)),
-    end: new Date(new Date().setHours(start.getHours() + 2))
+    end: new Date(new Date().setHours(start.getHours() + 2)),
   },
   {
     id: "3",
-    title: "Di don phong",
+    title: "Cerenle gezi",
     calendarId: "3",
     category: "time",
-    attendees: ["Hai"],
     isVisible: true,
     start: new Date(new Date().setHours(start.getHours() + 2)),
-    end: new Date(new Date().setHours(start.getHours() + 4))
+    end: new Date(new Date().setHours(start.getHours() + 4)),
   },
   {
     id: "4",
-    title: "Phai lam sao day",
+    title: "Barışla yemek",
     calendarId: "4",
     category: "time",
-    attendees: ["Linh"],
     isVisible: true,
     start: new Date(new Date().setHours(start.getHours() + 2)),
-    end: new Date(new Date().setHours(start.getHours() + 6))
-  }
+    end: new Date(new Date().setHours(start.getHours() + 6)),
+  },
 ];
+
 const colors = [
   {
     id: "1",
     color: "#ffffff",
     bgColor: "#34C38F",
     dragBgColor: "#34C38F",
-    borderColor: "#34C38F"
+    borderColor: "#34C38F",
   },
   {
     id: "2",
     color: "#ffffff",
     bgColor: "#F4696A",
     dragBgColor: "#F4696A",
-    borderColor: "#F4696A"
+    borderColor: "#F4696A",
   },
   {
     id: "3",
     color: "#ffffff",
     bgColor: "#00a9ff",
     dragBgColor: "#00a9ff",
-    borderColor: "#00a9ff"
+    borderColor: "#00a9ff",
   },
   {
     id: "4",
     color: "#ffffff",
     bgColor: "#F2B34C",
     dragBgColor: "#F2B34C",
-    borderColor: "#F2B34C"
+    borderColor: "#F2B34C",
   },
   {
     id: "5",
     color: "#ffffff",
     bgColor: "#74788D",
     dragBgColor: "#74788D",
-    borderColor: "#74788D"
+    borderColor: "#74788D",
   },
   {
     id: "6",
     color: "#ffffff",
     bgColor: "#343A40",
     dragBgColor: "#343A40",
-    borderColor: "#343A40"
+    borderColor: "#343A40",
   },
   {
     id: "7",
     color: "#000000",
     bgColor: "#FFFFFF",
     dragBgColor: "#FFFFFF",
-    borderColor: "#FFFFFF"
-  }
+    borderColor: "#FFFFFF",
+  },
 ];
 
 const calendars = [
   {
     id: "1",
-    name: "BPA Technical"
+    name: "Teknik",
   },
   {
     id: "2",
-    name: "Aqua 2 Cleaning"
+    name: "Temizlik",
   },
   {
     id: "3",
-    name: "Aqua 4 Cleaning"
+    name: "Aquapark",
   },
   {
     id: "4",
-    name: "Luxury 6 Cleaning"
+    name: "İş Toplantısı",
   },
   {
     id: "5",
-    name: "Luxury 6 Management"
+    name: "Yemek",
   },
   {
     id: "6",
-    name: "Aqua 3 Management"
+    name: "Gezi",
   },
   {
     id: "7",
-    name: "Aqua 2 Management"
-  }
+    name: "Tatil",
+  },
 ];
 
-function Calendar() {
+export default function Calendar() {
+  const { user, authenticated } = useUser();
   const [modal, setModal] = useState(false);
   const [event, setEvent] = useState(null);
+  const [data, setData] = useState(null);
   const childRef = useRef();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = getTokenFromLocalStorage();  
+
+        const response = await axios.get(API_ROUTES.GET_LIST_EVENT,{
+            headers: {
+              'Authorization': `Bearer ${token}`, 
+            }
+          });
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+        // handle error
+      }
+    };
+
+    fetchData();
+
+
+  },[]);
+
+  if (!user || !authenticated) {
+    return (
+      <div className="p-16 bg-gray-800 h-screen">
+        <div className="text-2xl mb-4 font-bold text-white">Dashboard</div>
+        <div className="ml-2 w-8 h-8 border-l-2 rounded-full animate-spin border-white" />
+      </div>
+    );
+  }
 
   const toggle = () => {
     setModal(!modal);
@@ -167,13 +192,14 @@ function Calendar() {
         ...event,
         id: schedules.length,
         title: newEvent.title,
+        description: newEvent.description,
         calendarId: newEvent.calendarId,
         category: event.isAllDay ? "allday" : "time",
-        attendees: newEvent.attendees,
+        
         isVisible: true,
         start: newEvent.start,
         end: newEvent.end,
-
+        remindDate: newEvent.remindDate,
         isAllDay: event.isAllDay,
         dueDateClass: "",
         location: event.location,
@@ -181,9 +207,9 @@ function Calendar() {
         //   class: event.raw["class"]
         // },
         state: event.state,
-        body: event.body
+        body: event.body,
       };
-
+      console.log(newSchedule)
       childRef.current.createSchedule(newSchedule);
       setModal(false);
     }
@@ -193,7 +219,7 @@ function Calendar() {
     // console.log('onBeforeUpdateSchedule', event)
 
     const { schedule, changes } = event;
-
+    console.log(event)
     // resize & drag n drop
     if (changes) {
       // call api
@@ -225,21 +251,21 @@ function Calendar() {
         ...event,
         id: schedules.length + 2,
         title: updateEvent.title,
+        description: updateEvent.description,
         calendarId: updateEvent.calendarId,
-        category: event.isAllDay ? "allday" : "time",
-        attendees: updateEvent.attendees,
+        category: "time",
         isVisible: true,
         start: updateEvent.start,
         end: updateEvent.end,
-
-        isAllDay: event.isAllDay,
+        remindDate: updateEvent.remindDate,
+        isAllDay: false,
         dueDateClass: "",
         location: event.location,
         // raw: {
         //   class: event.raw["class"]
         // },
         state: event.state,
-        body: event.body
+        body: event.body,
       };
 
       await childRef.current.createSchedule(newSchedule);
@@ -264,7 +290,7 @@ function Calendar() {
 
   const formatCalendars = calendars.map((element) => ({
     ...colors.find((element2) => element2.id === element.id),
-    ...element
+    ...element,
   }));
 
   return (
@@ -273,7 +299,7 @@ function Calendar() {
         ref={childRef}
         {...{
           isReadOnly: false,
-          showSlidebar: true,
+          showSlidebar: false,
           showMenu: true,
           useCreationPopup: false,
           // onCreate: () => {
@@ -285,7 +311,7 @@ function Calendar() {
           schedules,
           onBeforeCreateSchedule,
           onBeforeUpdateSchedule,
-          onBeforeDeleteSchedule
+          onBeforeDeleteSchedule,
         }}
       />
       <CustomTuiModal
@@ -298,14 +324,12 @@ function Calendar() {
               : handleUpdateSchedule,
           submitText: event?.triggerEventName === "mouseup" ? "Save" : "Update",
           calendars: formatCalendars,
-          attendees,
+          // attendees,
           schedule: event?.schedule,
           startDate: event?.start,
-          endDate: event?.end
+          endDate: event?.end,
         }}
       />
     </div>
   );
 }
-
-export default Calendar;
